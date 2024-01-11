981ec5dcb3c2fee48a7da45d527ee6a8158240a9
sendkite <sendkite1@gmail.com>
2024-01-11 21:36:11 +0900

    pins score

--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -1,9 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingTest {
 
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
     @Test
-    void nothing() {
+    void gameCanRoll() {
+        game.roll(0);
+    }
+
+    @Test
+    void gutterGame() {
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
+    }
+
+    @Test
+    void allOnes() {
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+        assertEquals(20, game.score());
     }
 }
-
