c4fd0bcea8f35818234b320a6bb54645152dd42b
sendkite <sendkite1@gmail.com>
2024-01-11 21:52:41 +0900

    refactor: change responsibility

--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -1,32 +1,44 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingTest {
 
-    private Game game;
+    private Game g;
 
     @BeforeEach
     void setUp() {
-        game = new Game();
+        g = new Game();
     }
 
     private void rollMany(int n, int pins) {
         for (int i = 0; i < n; i++) {
-            game.roll(pins);
+            g.roll(pins);
         }
     }
 
     @Test
-    void gutterGame() {
+    void gutterg() {
         rollMany(20, 0);
-        assertEquals(0, game.score());
+        assertEquals(0, g.score());
     }
 
     @Test
     void allOnes() {
         rollMany(20, 1);
-        assertEquals(20, game.score());
+        assertEquals(20, g.score());
+    }
+
+    @Disabled
+    @Test
+    void oneSpare() {
+        g.roll(5);
+        g.roll(5); // spare
+        g.roll(3);
+        rollMany(17, 0);
+
+        assertEquals(16, g.score());
     }
 }
